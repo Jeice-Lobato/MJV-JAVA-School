8a8ffed62af236fec8aa7344a6ec6bcf5131f0a4
Jeice Lobato <32601259+Jeice-Lobato@users.noreply.github.com>
2022-04-16 02:24:50 -0300

    AulaVariaveisString 14/04/2022

--- src/AulasVariaveis14042022.java
+++ src/AulasVariaveis14042022.java
@@ -4,7 +4,9 @@ public class AulasVariaveis14042022 {
 	public static void main(String[] args) {
      String nome = "Jeice";
      String sobreNome = "Lobato";
+     String idade = "ehSegredo";
      
-     System.out.println(nome + " " + sobreNome);
+     System.out.println("Nome: " + nome + " " + sobreNome);
+     System.out.println("Idade: " + idade);
 	}
 }
