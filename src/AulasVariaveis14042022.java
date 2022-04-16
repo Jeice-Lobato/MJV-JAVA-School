41bb48baae02c430b9377f7ca99fc088544b54e9
Jeice Lobato <32601259+Jeice-Lobato@users.noreply.github.com>
2022-04-16 02:10:40 -0300

    AulaVariaveis

---
+++ src/AulasVariaveis14042022.java
@@ -0,0 +1,10 @@
+
+
+public class AulasVariaveis14042022 {
+	public static void main(String[] args) {
+     String nome = "Jeice";
+     String sobreNome = "Lobato";
+     
+     System.out.println(nome + " " + sobreNome);
+	}
+}
--- src/aula101/MinhaClasse.java
+++ src/aula101/MinhaClasse.java
@@ -1,3 +1,4 @@
+package aula101;
 
 public class MinhaClasse {
   public static void main(String[] args) {
