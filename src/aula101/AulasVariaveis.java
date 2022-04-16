c71bf754825939ac5db524659c4eb44218442fcb
Jeice Lobato <32601259+Jeice-Lobato@users.noreply.github.com>
2022-04-16 02:59:08 -0300

    Brincando&PraticandoString

---
+++ src/aula101/AulasVariaveis.java
@@ -0,0 +1,20 @@
+package aula101;
+
+
+public class AulasVariaveis {
+	public static void main(String[] args) {
+      String fatiaPizza = "3/4";
+      System.out.println("- Quantas fatias de pizza você comeu? ");
+      System.out.println("R- Eu comi " + fatiaPizza + " da pizza, sinto muito!");
+      System.out.println("- Sem problemas, eu comprei mais!");
+      System.out.println(" ");
+      System.out.println("---Esse é apenas um diálogo que eu gostaria de ter, mas só ocorre em minha imaginação =( ---");
+     
+
+      
+		
+    		
+    
+   
+	}
+}
