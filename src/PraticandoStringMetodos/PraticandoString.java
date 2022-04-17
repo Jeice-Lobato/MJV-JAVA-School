f552c3ec765fd97a19e3f92b3c833a257550caf9
Jeice Lobato <32601259+Jeice-Lobato@users.noreply.github.com>
2022-04-17 00:39:31 -0300

    Exemplos simples de Métodos String

--- src/PraticandoStringMetodos/PraticandoString.java
+++ src/PraticandoStringMetodos/PraticandoString.java
@@ -4,11 +4,17 @@ public class PraticandoString {
 public static void main(String[] args) {
 	//String, MÈtodos e Sobrecarga.
 	
-	String frase = "SÛSeiQueNadaSei";
+	String frase = "EuAmoJava";
 	
 	int nome = frase.length();
-	System.out.println(nome);
+	String nome1 = frase.toUpperCase();
+	String nome2 = frase.toLowerCase();
+	String nome3 = frase.concat("Simplesmente" + frase);
 	
+	System.out.println(nome);
+	System.out.println(nome1);
+	System.out.println(nome2);
+	System.out.println(nome3);
 	
 	
   }
