3a536ad84535f5b2b85222f54a33fb5a204281b4
Jeice Lobato <32601259+Jeice-Lobato@users.noreply.github.com>
2022-04-17 17:50:53 -0300

    Exemplos simples de Método String 2

---
+++ src/praticandoMetodos17042022/praticandoMetodos.java
@@ -0,0 +1,11 @@
+package praticandoMetodos17042022;
+
+public class praticandoMetodos {
+public static void main(String[] args) {
+	String nome = "AIMÉE";
+	String nomeCorrigido = nome.toLowerCase();
+	 System.out.println(nomeCorrigido);
+	 System.out.println(nome);
+	 
+}
+}
