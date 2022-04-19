79e71f8a29718eb768703ed19c234511cb12de97
Jeice Lobato <32601259+Jeice-Lobato@users.noreply.github.com>
2022-04-18 22:20:44 -0300

    StringBuilder Métodos e Sobrecarga

--- src/Builder/StringBuilder4Métodos.java
+++ src/Builder/StringBuilder4Métodos.java
@@ -1,20 +1,21 @@
 package Builder;
-
+//Autora: Jeice Lobato / 17/04/2022
 public class StringBuilder4Métodos {
 public static void main(String[] args) {
+	
 	String s = "Viva a vida ";
-	StringBuilder sb = new StringBuilder();
+	StringBuilder sb = new StringBuilder();  //Instanciando a classe StringBuiler, criando então, um novo objeto.
 	
 	// Método append para concatenar "Strings" no StringBuilder.
 	sb.append("Viva a vida");
 	sb.append(" Sempre!");
-	
+
 	// Método to.String utilizado para retornar a String no StringBuilder.
 	s = sb.toString();
 	
 	System.out.println(s);
 	
-	// Método insert utilizado para inserir caracteres na posição indicada.
+	// Método insert utilizado para inserir caracteres na posição indicada, retorna a palavra com os caratreres inseridos.
 	System.out.println(sb.insert(4, " LA "));
 	
 	// Método delete utilizado para deletar caracteres nas posições indicadas.
@@ -22,6 +23,12 @@ public static void main(String[] args) {
 	
 	// Método reverse utilizado para inverter os caracteres.
 	System.out.println(sb.reverse());
+	
+	
+	 
+		// Exemplo de Sobrecarga: Quando os métodos são iguais e os parâmetros diferentes.
+		sb.append(123);
+		sb.append(false);
 	
 	
   }
