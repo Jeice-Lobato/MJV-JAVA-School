e210ff0e9497eb2a97a4d62c857f31208cc220a5
Jeice Lobato <32601259+Jeice-Lobato@users.noreply.github.com>
2022-04-24 01:13:33 -0300

    Água/Luz - Case1 - Endereço

---
+++ projeto-água-luz/src/main/java/com/mjv/aguaLuz/Endereco.java
@@ -0,0 +1,51 @@
+package com.mjv.aguaLuz;
+//23/04/2022
+//Aluna: Jeice Lobato
+public class Endereco {
+	private String logradouro;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String cep;
+	private String estado;
+	
+	
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+}
