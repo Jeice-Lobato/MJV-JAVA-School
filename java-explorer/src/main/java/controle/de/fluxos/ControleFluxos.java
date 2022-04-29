8d18d033fa1131f3c39dc36add82d4f7d83e7a2f
Jeice Lobato <32601259+Jeice-Lobato@users.noreply.github.com>
2022-04-29 20:17:42 -0300

    Estudos - Operadores

---
+++ java-explorer/src/main/java/controle/de/fluxos/ControleFluxos.java
@@ -0,0 +1,13 @@
+package controle.de.fluxos;
+//28/04/2022
+public class ControleFluxos {
+	public static void main(String[] args) {
+	 String concatenacao = "?";
+	 
+	 concatenacao = "1" + 1 + 1 + 1; 
+	 System.out.println(concatenacao);
+	}
+
+}
+
+//Quando temos uma string antes da soma dos números int/double, os números são repetidos sem somar.
